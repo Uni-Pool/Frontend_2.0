47dbc41be4eb5f4e1e01208fd23c55299a025f77
YoitsVansh <vansh.199301021@muj.manipal.edu>
2022-07-01 00:23:48 +0530

    Basic find UI complete

--- src/Pages/Find.js
+++ src/Pages/Find.js
@@ -3,7 +3,8 @@ import BottomNav from "../Components/BottomNav";
 import { FindForm } from "../Components/FindForm";
 import { Map } from "../Components/Map";
 import TopNav from "../Components/TopNav";
-import OR_BAR_FIND from '../assets/Find/OR_BAR_Find.png'
+import OR_BAR_Find from "../assets/Find/OR_BAR_Find.png";
+import OR_BAR_Create from "../assets/Find/OR_BAR_Create.png";
 
 function Find() {
 	return (
@@ -14,8 +15,22 @@ function Find() {
 					<Map />
 					<FindForm />
 				</div>
-				<img src = {OR_BAR_FIND}/>
-				
+				<img src={OR_BAR_Find} />
+				<div className='h-2/5 my-4 px-12 py-8 rounded-3xl bg-secondary10 shadow-md'>
+					<div className='h-1/2 flex flex-col items-center '>
+						<h3 className='text-center font-bold'> Create Your Own Pool</h3>
+						<button className='bg-primary px-6 py-2 rounded-full text-white mt-4 '>
+							Create a Pool
+						</button>
+					</div>
+					<img src={OR_BAR_Create} />
+					<div className='h-1/2 flex flex-col items-center'>
+						<h3 className='text-center font-bold pt-5'> Join an Existing Pool</h3>
+						<input type = "text" placeholder = "Type in your code here" className='bg-secondary50 text-center text-primary placeholder-primary px-6 py-2 rounded-full text-white mt-4 '/>
+							
+						
+					</div>
+				</div>
 			</div>
 			<BottomNav />
 		</div>
